bb5146702da7815114f28111b3d227be4b6c950c
Damian Paszkowski <paszko.damian@gmail.com>
2016-11-02 15:59:48 +0100

    Victory
    problem resolved

--- RTTI_lab/RTTI_lab/Source.cpp
+++ RTTI_lab/RTTI_lab/Source.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include "shape.h"
-#include "circle.h"
-#include "rectangle.h"
-#include "square.h"
+//#include <iostream>
+//#include "shape.h"
+//#include "circle.h"
+//#include "rectangle.h"
+//#include "square.h"
 
 int main()
 {
-	shape *sptr;
-	circle cir;
-	rectangle rec;
-	square sqr;
+	//shape *sptr;
+	//circle cir;
+	//rectangle rec;
+	//square sqr;
 
 
-	char user;
-	std::cin >> user;
+	//char user;
+	//std::cin >> user;
 
-	switch (user)
-	{
-	case 'a':
-		sptr = &cir;
-			break;
-	case 'b':
-		sptr = &rec;
-		break;
-	default:
-		sptr = &sqr;
-	}
+	//switch (user)
+	//{
+	//case 'a':
+	//	sptr = &cir;
+	//		break;
+	//case 'b':
+	//	sptr = &rec;
+	//	break;
+	//default:
+	//	sptr = &sqr;
+	//}
 
-	std::cout << "I'm " << typeid(sptr).name() << std::endl;
+	//std::cout << "I'm " << typeid(sptr).name() << std::endl;
 
 	return 0;
 }
