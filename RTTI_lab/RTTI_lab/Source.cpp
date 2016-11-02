2f7a4b920d8c167e8f60014c1c7814a02bb25df4
Damian Paszkowski <paszko.damian@gmail.com>
2016-11-02 16:04:51 +0100

    Finish

--- RTTI_lab/RTTI_lab/Source.cpp
+++ RTTI_lab/RTTI_lab/Source.cpp
@@ -1,33 +1,45 @@
-//#include <iostream>
-//#include "shape.h"
-//#include "circle.h"
-//#include "rectangle.h"
-//#include "square.h"
+#include <iostream>
+#include "shape.h"
+#include "circle.h"
+#include "rectangle.h"
+#include "square.h"
 
 int main()
 {
-	//shape *sptr;
-	//circle cir;
-	//rectangle rec;
-	//square sqr;
+	shape *sptr;
+	circle cir;
+	rectangle rec;
+	square sqr;
 
 
-	//char user;
-	//std::cin >> user;
+	char user;
+	std::cin >> user;
 
-	//switch (user)
-	//{
-	//case 'a':
-	//	sptr = &cir;
-	//		break;
-	//case 'b':
-	//	sptr = &rec;
-	//	break;
-	//default:
-	//	sptr = &sqr;
-	//}
+	switch (user)
+	{
+	case 'a':
+		sptr = &cir;
+			break;
+	case 'b':
+		sptr = &rec;
+		break;
+	default:
+		sptr = &sqr;
+	}
 
-	//std::cout << "I'm " << typeid(sptr).name() << std::endl;
+	std::cout << "I'm " << typeid(sptr).name() << std::endl;
+	std::cout << "I'm " << typeid(*sptr).name() << std::endl;
+
+	square* newSqr = dynamic_cast<square*>(sptr);
+	
+	if(newSqr)
+	{
+		newSqr->superMethod();
+	}
+	else
+	{
+		std::cout << "It can't be done" << std::endl; 
+	}
 
 	return 0;
 }
